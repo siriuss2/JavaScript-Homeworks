bd0b8fb74a7ee9fcf0927d21baa49644ca4f9487
Antonio <novoselskiantonio@gmail.com>
2022-12-09 19:40:33 +0100

    Homework#02

---
+++ Homework#01-Intro/script.js
@@ -0,0 +1,8 @@
+let phonePrice = 119.95;
+let phoneTask = 5.9;
+let phonesNumber = 30;
+
+let phonesValue = phonePrice - phoneTask;
+
+let priceValue = phonesValue * phonesNumber;
+console.log("Prices for all phones is", priceValue);
---
+++ Homework#02-Basics-Control/app.js
@@ -0,0 +1,38 @@
+let userInput = prompt("Enter the year");
+
+if((userInput-4)%12===0){
+   console.log("Rat");
+}
+else if((userInput-4)%12===1){
+    console.log("Ox");
+}
+else if((userInput-4)%12===2){
+    console.log("Tiger");
+}
+else if((userInput-4)%12===3){
+    console.log("Rabbit");
+}
+else if((userInput-4)%12===4){
+    console.log("Dragon");
+}
+else if((userInput-4)%12===5){
+    console.log("Snake");
+}
+else if((userInput-4)%12===6){
+    console.log("Horse");
+}
+else if((userInput-4)%12===7){
+    console.log("Goat");
+}
+else if((userInput-4)%12===8){
+    console.log("Monkey");
+}
+else if((userInput-4)%12===9){
+    console.log("Rooster");
+}
+else if((userInput-4)%12===10){
+    console.log("Dog");
+}
+else if((userInput-4)%12===11){
+    console.log("Pig");
+}
